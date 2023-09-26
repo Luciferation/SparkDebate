25aa8d2fc1585091b805344a5c508abcdaa95805
Orphea <3367436163@qq.com>
2023-09-26 16:19:57 +0800

    暂时把AIDebateTrain设为Home‘

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AIDebateTrain from '../views/AIDebateTrain/AIDebateTrain.vue';
 const routes = [
     {
+        path: '/',
+        name: 'Home',
+        component: AIDebateTrain
+    },
+    {
         path: '/AIDebateTrain',
         name: 'AIDebateTrain',
         component: AIDebateTrain
